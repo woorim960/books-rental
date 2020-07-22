6221bfc8567fd8dbd0141b3ed36e0988aeca5d81
박우림 <56839474+woorim960@users.noreply.github.com>
2020-07-22 16:02:58 +0900

    db.js 보안 설정

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'books-rental.cesobqa48tpp.ap-northeast-2.rds.amazonaws.com',
-  user: 'woorimprog',
-  password: 'zx1zx1zx1!',
-  database: 'books_rental'
+  host: 'books-rental.cesobqa48tpp.ap-northeast-2.rds.amazonaws.com', // DB 서버 도메인
+  user: 'woorimprog', // 지정한 유저 아이디
+  password: '', //  지정한 패스워드
+  database: 'books_rental' // 데이터 베이스 이름
 });
 
 db.connect();
